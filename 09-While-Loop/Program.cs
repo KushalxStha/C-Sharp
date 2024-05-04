a52e3aa66eb2ead440fd0b67a2790daa0f83ea83
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-04 18:59:41 +0545

    Loops

---
+++ 09-While-Loop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. While Loop
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine("i="+i);
+                i++;
+            }
+
+            // 2. Do While
+            int j = 0;
+            do
+            {
+                Console.WriteLine("j="+j);
+                j++;
+            } while (j < 5);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 10-For-Loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. For Loop
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // 2. For Each
+            string[] cars = { "BMW", "Toyota", "Mustang", "Tesla", "Ford" };
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
