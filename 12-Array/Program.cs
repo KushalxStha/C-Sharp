4c85cbebb895c01f279cd0379d6c493bef5288a3
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-08 20:54:17 +0545

    Array

---
+++ 12-Array/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] cars = new string[4];
+            //string[] cars = new string[4] { "Volvo", "BMW", "Ford", "Mazda" };
+            //string[] cars = new string[] { "Volvo", "BMW", "Ford", "Mazda" };
+
+            string[] cars = { "Hyundai", "Ford", "Tesla", "Suzuki", "BMW" };
+            int[] nums = { 5, 6, 3, 7, 8, 9 };
+
+            cars[0] = "Volvo";  // Changing Array Element
+            
+            Array.Sort(cars);  // Sorting: Array Method
+
+            // ForEach Loop
+            foreach (string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Array Methods that uses System.Linq
+            Console.WriteLine("Maximum element: "+nums.Max());
+            Console.WriteLine("Minimum element: "+nums.Min());
+            Console.WriteLine("Sum of elements: "+nums.Sum());
+            
+            Console.ReadLine();
+        }
+    }
+}
