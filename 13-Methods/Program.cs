d7afcbbfb7699a67e8478d4003d003977901b429
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-09 23:04:12 +0545

    New

---
+++ 13-Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heyy");
+            Console.ReadKey();
+            Console.WriteLine("How are you?");
+            Console.ReadLine();
+        }
+    }
+}
