fdad1dae58bd0f1597ac2fd0e3e81bd1e2cdc65a
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-15 13:50:31 +0545

    Params in Method

---
+++ 16-Params/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Params
+{
+    internal class Program
+    {
+        // Params keyword: A method parameter that takes a variable number of arguments.
+        //                 The parameter type must be single dimensional array.
+        static void Main(string[] args)
+        {
+            double Total = Checkout(15.6, 12.8, 13.9);
+
+            Console.WriteLine($"The total cost of products is {Total}");
+            Console.ReadKey();
+        }
+        
+        static double Checkout(params double[] prices)
+        {
+            double total = 0;
+            foreach (double pr in prices)
+            {
+                total += pr;
+            }
+            return total;
+        }
+    }
+}
