f04084ec2138d271b2836c13e4408f6207443254
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-16 19:57:56 +0545

    Classes

---
+++ 18-Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Class = A bundle of related codes.
+            //         Can be used as blueprints to create Objects (OOP)
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+            Console.ReadKey();
+        }
+    }
+}
