082b83f5427c33fe7b839bfaa38a1dbd044b774d
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-26 19:02:27 +0545

    Polymorphism

---
+++ 25-Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polymorphism = Greek word that means to "have many forms"
+            //                Objects can be identified by more than one type
+            //                Ex: A dog is also: Canine, Animal, Organism ...
+        }
+    }
+}
