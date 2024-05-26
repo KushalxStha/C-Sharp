1696d9829fa7d6af17ab9b4171ef75900f81195b
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-26 18:57:49 +0545

    Inheritance

---
+++ 22-Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _22_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.honk();
+            Console.WriteLine($"Car model is {car1.model}.");
+
+            Console.ReadKey();
+        }
+    }
+    // Base Class (Parent)
+    class Vehicle
+    {
+        public void honk()
+        {
+            Console.WriteLine("It can honk.");
+        }
+    }
+
+    // Derived Class (Child)
+    class Car: Vehicle
+    {
+        public string model="Ferrari";
+    }
+}
