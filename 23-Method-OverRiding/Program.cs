7d10a4aa0059d4a6d9634a6bc5046dd4ddaa167a
Kushal Shrestha <shresthakushal61@gmail.com>
2024-05-26 18:58:15 +0545

    Method overriding

---
+++ 23-Method-OverRiding/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Method_OverRiding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method Overriding = Provides a new version of a method Inherited from a parent class
+            //                     Inherited method must be: abstract, virtual, or already overriden
+            //                     Used with ToString(), polymorphism
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.speak();
+            cat.speak();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public virtual void speak()
+        {
+            Console.WriteLine("The animal goes ....");
+        }
+    }
+
+    class Dog: Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("The dog goes *woof*");
+        }
+    }
+
+    class Cat: Animal
+    {
+        public override void speak()
+        {
+            Console.WriteLine("The cat goes *meow*");
+        }
+    }
+}
