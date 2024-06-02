2eef46647006b8778d7578f888d487d56f852d9f
Kushal Shrestha <shresthakushal61@gmail.com>
2024-06-02 16:31:27 +0545

    Polymorphism

--- 25-Polymorphism/Program.cs
+++ 25-Polymorphism/Program.cs
@@ -1,18 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _25_Polymorphism
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             // Polymorphism = Greek word that means to "have many forms"
             //                Objects can be identified by more than one type
             //                Ex: A dog is also: Canine, Animal, Organism ...
+
+            Bike bike = new Bike();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Vehicle[] vehicle = { bike, bicycle, boat };
+
+            foreach (Vehicle v in vehicle)
+            {
+                v.Go();
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Vehicle
+    {
+        public virtual void Go()
+        {
+
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The car is moving.");
+        }
+    }
+    class Bicycle : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The bicycle is moving.");
+        }
+    }
+    class Boat : Vehicle
+    {
+        public override void Go()
+        {
+            Console.WriteLine("The boat is moving.");
         }
     }
 }
