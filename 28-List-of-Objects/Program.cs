ba9f7c1174bfbc3eb6c03b949fd973c4e956ad4c
Kushal Shrestha <shresthakushal61@gmail.com>
2024-06-13 21:04:16 +0545

    List of Objects

---
+++ 28-List-of-Objects/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _28_List_of_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Player> players = new List<Player>();
+
+            Player player1 = new Player("Ram");
+            Player player2 = new Player("Hari");
+            //Player Player3 = new Player("Shyam");
+
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(new Player("Shyam"));  // Anonymous Object
+
+            foreach (Player p in players)
+            {
+                // Console.WriteLine(p.name); Not necessary bcz of overrided ToString()
+                Console.WriteLine(p);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class Player
+    {
+        public string name;
+
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
