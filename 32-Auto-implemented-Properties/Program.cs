aa8fc603cbe81ed4c4d49e62a21e8a4b45de2edc
Kushal Shrestha <shresthakushal61@gmail.com>
2024-06-14 15:06:01 +0545

    Auto implemented properties

---
+++ 32-Auto-implemented-Properties/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _32_Auto_implemented_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("BMW");
+            Console.WriteLine(car1.Model);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        //private string model;
+        //public string Model
+        //{
+        //    get { return model; }
+        //    set { model = value; }
+        //}
+
+        public string Model { get; set; }  // Shortcut for above
+
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+    }
+}
